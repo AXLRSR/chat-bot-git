3dbb37dd6ce5bd6dd50574e04ff8a53897424932
AXLRSR <62110917+AXLRSR@users.noreply.github.com>
2020-09-22 11:58:34 +0200

    refactor(js): Add comments and optimization

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
-	// Disable the refresh on submit
+// Disable the refresh on submit
 document.getElementById("form").addEventListener("click", function(event){
   event.preventDefault()
 });
 
+// List of bots
 var bot = [{
   id: 0,
   name: 'BOT Roll',
@@ -10,22 +11,21 @@ var bot = [{
   counter: 0
 }];
 
-document.onload = displayBot();
-
+// Display all the bots in the contact list
 function displayBot() {
   var contact = document.querySelector(".contact");
   for (let i = 0; i < bot.length; i++) {
-    contact.innerHTML += `
-    <div class="contact__profile contact__profile--active">
+    contact.innerHTML += `<div class="contact__profile">
       <div class="contact__picture">
         <img src="./assets/img/bot`+ bot[i].id +`.png" alt="bot" class="contact__picture__img">
       </div>
       <span class="contact__name">`+ bot[i].name +`</span>
-      <span class="contact__counter">`+ bot[i].counter +`</span>
+      <span class="contact__counter">0</span>
     </div>`
   }
 }
 
+document.onload = displayBot();
 
 function sendMessage(){
   var inputVal = document.getElementById("message").value; // Retrieve the user input
@@ -47,34 +47,23 @@ function sendMessage(){
 }
 
 function counterBot(id){
+  var counterId = document.querySelectorAll(".contact__counter")[id];
+  var counter = counterId.textContent;
 
-		var counterId = document.querySelectorAll(".contact__counter")[id];
-		var counter = counterId.textContent;
-
-		counter = Number(counter);
-		counter += 1;
-		counterId.textContent = counter;
-
+  counter = Number(counter);
+  counter += 1;
+  counterId.textContent = counter;
 }
 
-/*
-testing if the message sent is a bot action and send the corresponding message
-*/
-
+// Test if the message sent is a bot action and send the corresponding message
 function botTest(inputVal) {
+  switch (inputVal){
+    case bot[0].action : 
+      // Increment the counter
+      var counter = bot[0].id;
+      counterBot(counter);
 
-	bot.forEach(function (thisBot){
-		if (inputVal === thisBot.action){
-			switch (inputVal){
-				case '/roll' : 
-					var reponseCompteur = thisBot.id;
-					counterBot(reponseCompteur);
-					randomRoll = Math.floor((Math.random()*99));
-					return console.log(randomRoll);
-					break;
-			}
-		}
-	});
-
+      randomRoll = Math.floor((Math.random()*99));
+      return randomRoll;
+  }
 }
-
